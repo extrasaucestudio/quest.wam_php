23983a1b1eabdf1c704f9416df3e8df2796b0811
slikhub <clonage@gmail.com>
2022-06-26 22:53:37 -0400

    5454

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'workerman/browserquest',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4a4734f2373761ac182574206a7fab373f4c474e',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'workerman/browserquest' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4a4734f2373761ac182574206a7fab373f4c474e',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'workerman/workerman' => array(
+            'pretty_version' => 'v4.0.39',
+            'version' => '4.0.39.0',
+            'reference' => '97ff0d0d3d20ce9abfd0f3c87e5e3481d3bf16d9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../workerman/workerman',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
